cd266e36db51ef8cbaf9e1703d76c28ed55a55a4
psp10 <psp10@SHAHAD>
2024-12-16 22:53:18 +0300

    change

--- theProject/Program.cs
+++ theProject/Program.cs
@@ -17,25 +17,18 @@ Console.WriteLine(converstion1); // here a new variable has been created an inte
 double converstion2 = Convert.ToDouble(thisInt);
 Console.WriteLine(converstion2); // here a new variable has been created a double of thisInt
 
-
-
-
-Console.WriteLine("\n\n==================================== Bonus 1: ==================================");
+//Console.WriteLine("\n\n==================================== Bonus 1: ==================================");
 //char is used to declare one character be it a (number, special character, letter)
 //char is more memory efficient than a string 
-//since it takes a small place in memore, char makes the code run faster.
 //char can be used to manipulate strings and extracting a single character out of them 
 
 Console.WriteLine("\n\n================================ C# variables: =================================");
 Console.WriteLine("==================================== Task 1: ===================================");
-
 string studentName; // Camel case
 double grade_point_average; // Underscoring / Snake case
 string FavoriteSubject; // Pascal case
-
 // If we wanted to print it for output asthetics (uncomment the following):
 //Console.WriteLine("studentName ->  Camel case \ngrade_point_average -> Underscoring / Snake Case \nFavoriteSubject -> Pascal case");
-
 
 Console.WriteLine("\n==================================== Task 2: ===================================");
 double Area;
@@ -44,9 +37,8 @@ double Length = 5;
 Area = Width * Length;
 Console.WriteLine("Area is: " + Area);
 
-
-Console.WriteLine("\n\n==================================== Bonus 1: ==================================");
-int variable = 5; // a variable could be possibly change later on (immutable)
+Console.WriteLine("\n\n==================================== Bonus 2: ==================================");
+int variable = 5; //a variable could be possibly change later on (immutable)
 Console.WriteLine("enter a number");
 const int constant = 6; // a constant is declared and can not be changed later on (mutable)
 Console.WriteLine("the constant is :" + constant + "\tthe variable is " + variable);
